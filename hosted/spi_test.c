ab15e72cb27686d318ecfa678909488bee3631a9
tiniuclx <tiniuc.alexandru@gmail.com>
2017-05-20 00:59:23 +0000

    Tweaked things around.

--- hosted/spi_test.c
+++ hosted/spi_test.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define CHANNEL 0
+#define CE0 11 // Chip Enable pin
 
 int main() {
 	/*
@@ -13,13 +14,19 @@ int main() {
 	 * Restart to complete the process.
 	 * */
 	wiringPiSetup();
-	wiringPiSPISetup(CHANNEL, 750000);
+	wiringPiSPISetup(CHANNEL, 500000);
+	pinMode(CE0, OUTPUT);
+	digitalWrite(CE0, 1);
 	unsigned char buffer[256];
 	while(1){
 		printf("Send over SPI:     ");
 		if(!scanf("%s", buffer))
 			printf("\nError reading from stdin!");
-		wiringPiSPIDataRW(CHANNEL, buffer, 256);
+		digitalWrite(CE0, 0);
+		delay(10);
+		wiringPiSPIDataRW(CHANNEL, buffer, strlen(buffer)+1);
+		delay(10);
+		digitalWrite(CE0, 1);
 		printf("Received over SPI: %s\n", buffer);		
 		memset(buffer,0,sizeof(buffer));
 	}
